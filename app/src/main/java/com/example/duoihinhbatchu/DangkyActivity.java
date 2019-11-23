d3d2ace40b0d28f96b09ca501179917090d542ad
phamducthang123 <thangpdph09030@fpt.edu.vn>
2019-11-23 20:13:07 +0700

    abc

--- app/src/main/java/com/example/duoihinhbatchu/DangkyActivity.java
+++ app/src/main/java/com/example/duoihinhbatchu/DangkyActivity.java
@@ -9,6 +9,6 @@ public class DangkyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dangky);//sdaads
+        setContentView(R.layout.activity_dangky);
     }
 }
